f09ed7eb667bb17e0d14453b304eb087d0f597bb
haiduong312 <duonghai03122004@gmail.com>
2025-09-30 02:07:52 +0700

    header component

--- src/components/header/app.header.tsx
+++ src/components/header/app.header.tsx
@@ -1,33 +1,84 @@
 "use client";
 
-import { Layout, Menu } from "antd";
-
+import {
+  BarsOutlined,
+  BellOutlined,
+  NotificationOutlined,
+  QuestionCircleOutlined,
+  SearchOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import { Avatar, Button, Input, Layout } from "antd";
+import "@/components/styles/header.style.scss";
+import Image from "next/image";
 export default function AppHeader() {
-    return (
-        <Layout>
-            <Layout.Header style={{ display: "flex", alignItems: "center" }}>
-                <div
-                    style={{
-                        color: "white",
-                        fontWeight: "bold",
-                        fontSize: "20px",
-                        marginRight: "20px",
-                    }}
-                >
-                    Trello Clone
-                </div>
-                <Menu
-                    theme="dark"
-                    mode="horizontal"
-                    defaultSelectedKeys={["1"]}
-                    items={[
-                        { key: "1", label: "Dashboard" },
-                        { key: "2", label: "Boards" },
-                        { key: "3", label: "Profile" },
-                    ]}
-                    style={{ flex: 1, minWidth: 0 }}
-                />
-            </Layout.Header>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <Layout.Header
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          padding: 8,
+          background: "white",
+          height: 48,
+        }}
+      >
+        <div
+          style={{
+            color: "white",
+            fontWeight: "bold",
+            fontSize: "20px",
+            marginRight: "20px",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <div className="options-icon">
+            <BarsOutlined
+              style={{
+                color: "rgba(80,82,88,1)",
+                fontSize: 20,
+                paddingLeft: 10,
+                paddingRight: 10,
+              }}
+            />
+          </div>
+          <div className="app-icon">
+            <Image
+              src="/assets/trello-icon.svg"
+              alt="trello"
+              width={25}
+              height={25}
+            />
+            <div className="title">Trello</div>
+          </div>
+        </div>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <Input
+            placeholder="Search"
+            prefix={<SearchOutlined />}
+            style={{ width: "780px" }}
+          />
+          <Button style={{ marginLeft: 8 }} type="primary">
+            Search
+          </Button>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <NotificationOutlined style={{ fontSize: 20 }} />
+          <BellOutlined style={{ fontSize: 20, paddingLeft: 15 }} />
+          <QuestionCircleOutlined
+            style={{ fontSize: 20, paddingLeft: 15, paddingRight: 15 }}
+          />
+          <Avatar icon={<UserOutlined />} size={25} />
+        </div>
+      </Layout.Header>
+    </Layout>
+  );
 }
